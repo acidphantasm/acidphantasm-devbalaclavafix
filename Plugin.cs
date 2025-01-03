1ee919d3465ab8db5cd24d1813af9fe567c99e57
acidphantasm <acidphantasm@acidphantasm.com>
2025-01-03 14:25:43 -0600

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using acidphantasm_devbalaclavafix.Patches;
+using BepInEx;
+
+namespace acidphantasm_devbalaclavafix
+{
+    [BepInPlugin("phantasm.acid.devbalaclavafix", "acidphantasm-DevBalaclavaFix", "1.0.0")]
+    [BepInDependency("com.SPT.core", "3.10.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("[DevBalaclavaFix] loading...");
+
+            new ApplyDamageInfoPrefixPatch().Enable();
+
+            Logger.LogInfo("[DevBalaclavaFix] loaded!");
+        }
+    }
+}
